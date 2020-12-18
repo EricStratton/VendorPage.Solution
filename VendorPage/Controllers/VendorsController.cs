44f2cc3ec4001138b010645726aeff3626a0d9b6
Eric Stratton <strattonericj@gmail.com>
2020-12-18 09:38:49 -0800

    add Vendor controller and index page and path on Home view

---
+++ VendorPage/Controllers/VendorsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorPage.Models;
+
+namespace VendorPage.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
