b228c553ef8cc0d3365c610b6ca49dce1c6d5538
Eric Stratton <strattonericj@gmail.com>
2020-12-18 11:10:35 -0800

    add object constructor and test to return object description, good fail

--- VendorPage.Tests/ModelTests/OrderTests.cs
+++ VendorPage.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,17 @@ namespace VendorPage.Tests
     [TestMethod]
     public void OrderContructor_CreatesIntanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Test Description");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetOrderDescription_ReturnsOrderDescription_String()
+    {
+      string description = "Baked goods order";
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }
--- VendorPage/Models/Order.cs
+++ VendorPage/Models/Order.cs
@@ -4,6 +4,12 @@ namespace VendorPage.Models
 {
   public class Order
   {
-    
+    public string Description { get; set; }
+
+    public Order(string description)
+    {
+      Description = "Not Bakery order";
+    }
+
   }
 }
