82e6ab45cba0e7ad86f3ba21dd80e86349150f9c
Eric Stratton <strattonericj@gmail.com>
2020-12-18 08:56:05 -0800

    pass Vendor Id test

--- VendorPage/Models/Vendor.cs
+++ VendorPage/Models/Vendor.cs
@@ -9,12 +9,14 @@ namespace VendorPage.Models
       public string Name { get; set; }
       public string Description { get; set; }
       public int Id { get; }
+      private List<Vendor> _instances = new List<Vendor> {};
 
       public Vendor(string vendorName, string vendorDescription)
       {
         Name = vendorName;
         Description = vendorDescription;
-        Id = 5;
+        _instances.Add(this);
+        Id = _instances.Count;
       }
   }
 }
