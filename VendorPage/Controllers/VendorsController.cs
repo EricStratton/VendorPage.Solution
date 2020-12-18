e6f6f0d487e8f401d4ed88fb2d6e5ad7920ce31b
Eric Stratton <strattonericj@gmail.com>
2020-12-18 10:20:41 -0800

    add show route to vendors controller and corresponding view file

--- VendorPage/Controllers/VendorsController.cs
+++ VendorPage/Controllers/VendorsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using VendorPage.Models;
 
 namespace VendorPage.Controllers
@@ -8,7 +9,8 @@ namespace VendorPage.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      return View();
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
 
     [HttpGet("/vendors/new")]
@@ -17,5 +19,20 @@ namespace VendorPage.Controllers
       return View();
     }
 
+    [HttpPost("/vendors")] // Create a vendor profile
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index", newVendor);
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      model.Add("vendor", selectedVendor);
+      return View(model);
+    }
   }
 }
